0d0909a9cdb976c2ed672020ca36dd986cac5702
Toginomi <rudyiipilande@gmail.com>
2025-10-04 23:02:36 +0800

    week 3 team

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,9 @@
+import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
 
-// load the header and footer
 loadHeaderFooter();
 
-// create the data source
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const productList = new ProductList("Tents", dataSource, element);
